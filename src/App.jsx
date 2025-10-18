f09023d1e777718967750c73cf28181b6e102ca8
Tahreem <tahreemnoor86@gmail.com>
2025-10-19 01:18:31 +0500

    frontend: added pages and routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-out' element={<SignOut />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
